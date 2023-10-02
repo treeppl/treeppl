0c20a9f250a7a852febc7501d31836cfc736da19
Jan Kudlicka <github@kudlicka.eu>
2023-10-02 15:09:15 +0200

    Added setup.py.

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='treeppl',
+    version='0.1',
+    description='Python Interface to TreePPL',
+    author='Jan Kudlicka',
+    author_email='github@kudlicka.eu',
+    packages=['treeppl'],
+    install_requires=[
+        'numpy',
+    ],
+)
